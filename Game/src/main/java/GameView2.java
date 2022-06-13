f743f3c5caac991755f86e3ee74af29c4d27b91b
lukaszparadylo <lukaszparadylo@gmail.com>
2022-06-13 20:28:49 +0200

    This implementation is working

--- Game/src/main/java/GameView2.java
+++ Game/src/main/java/GameView2.java
@@ -8,32 +8,27 @@ import pl.lukaszparadylo.gamers.Gamer;
 import pl.lukaszparadylo.gamers.Robot2;
 import pl.lukaszparadylo.judge.Judge2;
 
-import java.time.LocalTime;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class GameView2 {
-    /*@FXML Button _01_5,_02_5,_03_5,_04_5,_05_5,_06_5,_07_5,_08_5,_09_5,_10_5,_11_5,_12_5,_13_5,_14_5,
-            _15_5,_16_5,_17_5,_18_5,_19_5,_20_5,_21_5,_22_5,_23_5,_24_5,_25_5;
-    @FXML Button _01_4,_02_4,_03_4,_04_4,_05_4,_06_4,_07_4,_08_4,_09_4,_10_4,_11_4,_12_4,_13_4,_14_4,
-            _15_4,_16_4;
-    @FXML Button _01_3,_02_3,_03_3,_04_3,_05_3,_06_3,_07_3,_08_3,_09_3;*/
+
     @FXML Button start, nextMove, restart, newGame;
     @FXML ChoiceBox choiceBoxGamer1, choiceBoxGamer2, boardSizeChoiceBox;
     @FXML TextField textFieldGamer1,textFieldGamer2;
     @FXML AnchorPane anchorPane;
-    @FXML CheckBox xShape1, oShape1,xShape2, oShape2, levelEasy1, levelMedium1, levelHard1, levelEasy2, levelMedium2, levelHard2;
+    @FXML CheckBox xShape1, oShape1,xShape2, oShape2, levelEasy1, levelMedium1, levelHard1,
+            levelEasy2, levelMedium2, levelHard2;
     @FXML Label infoLabel, scoreGamer1, scoreGamer2, roundNumber, moveNumber, winnerLabel;
 
     public static final String board3x3 = "3x3";
     public static final String board5x5 = "5x5";
     public static final String board4x4 = "4x4";
-    public Integer boardSize;
     public static final String GAMER = "Gamer";
     public static final String ROBOT = "Robot";
-    private boolean timerIsOnLine = false;
-
+    public Integer boardSize;
+    private Integer roundNumberInt = 1;
 
     private Judge2 judge2;
     private Board board;
@@ -41,48 +36,45 @@ public class GameView2 {
     private Gamer gamer2;
     private Robot2 robot1;
     private Robot2 robot2;
+    private Integer scoreGamer1int=0, scoreGamer2int=0;
 
-    public void initTempData(){
-        boardSize = 5;
-        board = new Board(boardSize);
-        gamer1 = new Gamer("juzek", 'X');
-        gamer2 = new Gamer("fff",'O');
-        robot1 = new Robot2('X',1);
-        robot2 = new Robot2('O',1);
-    }
+    List<Robot2> robotsList;
+    List<Gamer> gamersList = new ArrayList<>();
+
     public void startGame(){
-        if(boardSizeChoiceBox.getValue()==null){
+        roundNumber.setText(roundNumberInt.toString());
+        if(boardSizeChoiceBox.getValue()==null) {
             infoLabel.setText("Set board size !");
         }else if(choiceBoxGamer1.getValue()==null||choiceBoxGamer2.getValue()==null){
             infoLabel.setText("Select type of gamer !");
-        }else if((xShape1.isSelected()||oShape1.isSelected()==false)&&
-                (xShape2.isSelected()||oShape2.isSelected()==false)){
+        }else if((xShape1.isSelected()||!oShape1.isSelected())&& (xShape2.isSelected()||!oShape2.isSelected())){
             infoLabel.setText("Choose mark for gamer !");
         }else if(!(levelEasy1.isSelected()||levelMedium1.isSelected()||levelHard1.isSelected())
                 &&choiceBoxGamer1.getValue()==ROBOT){
             infoLabel.setText("Choose robot level !");
-        }
-        else if(!(levelEasy2.isSelected()||levelMedium2.isSelected()||levelHard2.isSelected())&&
-        choiceBoxGamer2.getValue()==ROBOT){
+        }else if(!(levelEasy2.isSelected()||levelMedium2.isSelected()||levelHard2.isSelected())&&
+            choiceBoxGamer2.getValue()==ROBOT){
             infoLabel.setText("Choose robot level !");
         }
         else {
-            Character gamer1Mark='_';
-            Character gamer2Mark='_';
+            Character gamer1Mark;
+            Character gamer2Mark;
             Integer robot1Level = 1;
             Integer robot2Level = 1;
-            if(xShape1.isSelected()==true) gamer1Mark='X';
+            enableButtons();
+            if(xShape1.isSelected()) gamer1Mark='X';
             else gamer1Mark='O';
-            if (xShape2.isSelected()==true) gamer2Mark='X';
+            if (xShape2.isSelected()) gamer2Mark='X';
             else gamer2Mark='O';
+
             if(choiceBoxGamer1.getValue()==ROBOT&&levelHard1.isSelected()) robot1Level=1;
             if(choiceBoxGamer1.getValue()==ROBOT&&levelMedium1.isSelected()) robot1Level=2;
             if(choiceBoxGamer1.getValue()==ROBOT&&levelEasy1.isSelected()) robot1Level=3;
             if(choiceBoxGamer2.getValue()==ROBOT&&levelHard2.isSelected()) robot1Level=1;
             if(choiceBoxGamer2.getValue()==ROBOT&&levelMedium2.isSelected()) robot1Level=2;
             if(choiceBoxGamer2.getValue()==ROBOT&&levelEasy2.isSelected()) robot1Level=3;
-            boardSize=Integer.parseInt(boardSizeChoiceBox.getValue().toString().substring(0,1));
-            infoLabel.setText(boardSize.toString());
+
+            boardSize = Integer.parseInt(boardSizeChoiceBox.getValue().toString().substring(0,1));
             board = new Board(boardSize);
             judge2 = new Judge2(board);
             if(choiceBoxGamer1.getValue()==ROBOT) {
@@ -93,100 +85,174 @@ public class GameView2 {
                 gamer1 = new Gamer(textFieldGamer1.getText(),gamer1Mark);
                 gamer1.setNextMove(true);
             }
-            if(choiceBoxGamer2.getValue()==ROBOT) robot2 = new Robot2(gamer2Mark,robot2Level);
-            if (choiceBoxGamer2.getValue()==GAMER) gamer2 = new Gamer(textFieldGamer2.getText(),gamer2Mark);
-            hideButtons();
+            if(choiceBoxGamer2.getValue()==ROBOT) {
+                robot2 = new Robot2(gamer2Mark,robot2Level);
+            }
+            if (choiceBoxGamer2.getValue()==GAMER) {
+                gamer2 = new Gamer(textFieldGamer2.getText(),gamer2Mark);
+            }
+            robotsList = new ArrayList<>();
+            robotsList.add(robot1);
+            robotsList.add(robot2);
+            gamersList = new ArrayList<>();
+            gamersList.add(gamer1);
+            gamersList.add(gamer2);
+            infoLabel.setText(boardSize.toString());
             setVisibleButtons();
             setNewBoard();
             nextMoveFinishGame();
             disableStartControls();
             start.setDisable(true);
+            //nextMove.setDisable(false);
         }
     }
-    public void nextMoveFinishGame(){
+    public void robotVsRobot(){
 
-        String wonCombination = judge2.checkWhoWon();
-        if(robot1!=null){
-            if(robot1.getNextMove()){
-                robot1.makeMove(board);
-                enableButtons();
+    }
+    public  void robotVsHuman(){
+
+    }
+    public void humanVsHuman(){
+
+    }
+    public void nextMoveFinishGame(){
+        nextMove.setDisable(false);
+        for (Robot2 robot : robotsList) {
+            if(robot!=null&&robot.getNextMove()){
+                robot.makeMove(board);
                 moveToggle();
-                nextMove.setDisable(true);
-            }
-            if(wonCombination==robot1.getChosenMark().toString()){
-                theWinner(robot1.getName());
+                disableButtons();
+                nextMove.setDisable(false);
+                if(judge2.checkWhoWon().equals(robot.getChosenMark().toString())){
+                    theWinner();
+                }
+                else if(board.getMoveNumber()>=boardSize*boardSize){
+                    draw();
+                }
+                break;
             }
         }
-        if(robot2!=null){
-            if(robot2.getNextMove()){
-                robot2.makeMove(board);
-                enableButtons();
-                moveToggle();
-                nextMove.setDisable(true);
-            }
-            if(wonCombination==robot2.getChosenMark().toString()){
-                theWinner(robot2.getName());
-            }
+        if((robot1!=null&&gamer1!=null)||(robot1!=null&&gamer2!=null)||
+                (robot2!=null&&gamer1!=null)||(robot2!=null&&gamer2!=null)){
+            enableButtons();
+            nextMove.setDisable(true);
+        }
+        if(gamer1!=null&&gamer2!=null) {
+            nextMove.setDisable(true);
         }
         board.incrementMoveNumber();
         moveNumber.setText(board.getMoveNumber().toString());
         insertDataFromBoardToScreen();
+        if(!judge2.checkWhoWon().equals("draw")){
+            theWinner();
+        }
+        else if(board.getMoveNumber()>=boardSize*boardSize){
+            draw();
+        }
+
     }
     public void setNewGame(){
+        roundNumberInt++;
+        roundNumber.setText(roundNumberInt.toString());
         uncheckControls();
         enableStartControls();
+        hideButtons();
         start.setDisable(false);
+        winnerLabel.setText("");
+        enableButtons();
+
     }
-    private void theWinner(String winner){
+    public void resetButton(){
+        roundNumberInt++;
+        roundNumber.setText(roundNumberInt.toString());
+        hideButtons();
+        start.setDisable(false);
+        winnerLabel.setText("");
+        enableButtons();
+        startGame();
+        insertDataFromBoardToScreen();
+        if(choiceBoxGamer1.getValue().equals(ROBOT)&&choiceBoxGamer2.getValue().equals(ROBOT)){
+            disableButtons();
+        }
+        else if(choiceBoxGamer1.getValue().equals(GAMER)&&choiceBoxGamer2.getValue().equals(GAMER)){
+            enableButtons();
+        }
+        else {
+            enableButtons();
+        }
+        start.setDisable(true);
+        //nextMove.setDisable(false);
+        /*setVisibleButtons();
+        insertDataFromBoardToScreen();
+        moveNumber.setText(board.getMoveNumber().toString());
+        winnerLabel.setText("");
+        board.showBoard();*/
+    }
+    private void draw(){
+        hideButtons();
+        winnerLabel.setText("DRAW");
+        nextMove.setDisable(true);
+    }
+    private void theWinner(){
+        String winner="";
+        if(board.getMoveNumber()%2==0){
+            winner=textFieldGamer2.getText();
+            scoreGamer2int++;
+            scoreGamer2.setText(scoreGamer2int.toString());
+        }
+        else {
+            winner=textFieldGamer1.getText();
+            scoreGamer1int++;
+            scoreGamer1.setText(scoreGamer1int.toString());
+        }
         hideButtons();
         winnerLabel.setText("The winner is "+winner);
-        //roundNumber.setText(board.);
+        nextMove.setDisable(true);
     }
     public void insertDataToBoard(ActionEvent event){
-
-        String wonCombination = judge2.checkWhoWon();
         Integer s = Integer.parseInt(event.getSource().toString().substring(11,13));
-        if(gamer1!=null){
-            if(gamer1.getNextMove()){
-                board.setFields(s,gamer1.getChosenMark());
+        for (Gamer gamer : gamersList) {
+            if(gamer!=null&&gamer.getNextMove()){
+                board.setFields(s,gamer.getChosenMark());
                 enableButtons();
-                moveToggle();
-                nextMove.setDisable(false);
-            }
-            if(wonCombination==gamer1.getChosenMark().toString()){
-                theWinner(gamer1.getName());
+                if(judge2.checkWhoWon().equals(gamer.getChosenMark().toString())){
+                    theWinner();
+                }
+                else if(board.getMoveNumber()>=boardSize*boardSize){
+                    draw();
+                }
             }
         }
-        if(gamer2!=null){
-            if(gamer2.getNextMove()){
-                board.setFields(s,gamer2.getChosenMark());
-                enableButtons();
-                moveToggle();
-                nextMove.setDisable(false);
-            }
-            if(wonCombination==gamer2.getChosenMark().toString()){
-                theWinner(gamer2.getName());
-            }
+        board.incrementMoveNumber();
+        moveNumber.setText(board.getMoveNumber().toString());
+        moveToggle();
+        if((robot1!=null&&gamer1!=null)||(robot1!=null&&gamer2!=null)||
+                (robot2!=null&&gamer1!=null)||(robot2!=null||gamer2!=null)){
+            disableButtons();
+            nextMove.setDisable(false);
+        }
+        if(gamer1!=null&&gamer2!=null){
+            enableButtons();
+            nextMove.setDisable(true);
         }
         insertDataFromBoardToScreen();
-        disableButtons();
-        if(gamer1!=null&&gamer2!=null)enableButtons();
     }
+
     private void moveToggle(){
         if(gamer1!=null){
-            if(gamer1.getNextMove()==true)gamer1.setNextMove(false);
+            if(gamer1.getNextMove())gamer1.setNextMove(false);
             else gamer1.setNextMove(true);
         }
         if(gamer2!=null){
-            if(gamer2.getNextMove()==true)gamer2.setNextMove(false);
+            if(gamer2.getNextMove())gamer2.setNextMove(false);
             else gamer2.setNextMove(true);
         }
         if(robot1!=null){
-            if(robot1.getNextMove()==true)robot1.setNextMove(false);
+            if(robot1.getNextMove())robot1.setNextMove(false);
             else robot1.setNextMove(true);
         }
         if(robot2!=null){
-            if(robot2.getNextMove()==true)robot2.setNextMove(false);
+            if(robot2.getNextMove())robot2.setNextMove(false);
             else robot2.setNextMove(true);
         }
     }
@@ -230,13 +296,13 @@ public class GameView2 {
         for (int i = 0; i < boardSize*boardSize; i++) {
             Integer buttonId = Integer.parseInt(buttonsList.get(i).getId().substring(1,3));
             buttonsList.get(i).setText(board.getFieldByPosition(buttonId).toString());
+            if(buttonsList.get(i).getText().equals("X")||buttonsList.get(i).getText().equals("O")){
+                System.out.println("insertDataFromBoardToScreen "+i);
+                buttonsList.get(i).setDisable(true);
+            }
         }
     }
-    public void resetButton(){
-        board = new Board(board.getBoardSize());
-        insertDataFromBoardToScreen();
-        nextMoveFinishGame();
-    }
+
     public void chooseMark1X(){
         xShape2.setSelected(false);
         oShape1.setSelected(false);
